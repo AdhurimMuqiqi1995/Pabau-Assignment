f1be8d711635c09d73c90c3b71ec6a0b88bd5d27
Adhurim Muqiqi <adhurimmuqiqi1995@gmail.com>
2023-06-24 01:29:56 +0200

    Created Next.js app, installed Tailwind CSS, created components, organized project structure, and imported components into index.js

---
+++ monorepo/React/nextjs-task/services/optionsData.js
@@ -0,0 +1,15 @@
+const options = [
+    { id: 1, name: 'Dermal Fillers' },
+    { id: 2, name: 'Secret RF' },
+    { id: 3, name: 'Facials' },
+    { id: 4, name: 'Led' },
+    { id: 5, name: 'Fat Dissolve' },
+    { id: 6, name: 'ProfHilo' },
+    { id: 7, name: 'Growth Factors' },
+    { id: 8, name: 'HArmonyCA' },
+    { id: 9, name: 'Special Package for Time Clinic' },
+    { id: 10, name: 'Consulation' },
+  ];
+  
+  export default options;
+  

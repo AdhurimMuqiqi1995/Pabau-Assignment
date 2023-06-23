fc064821a12ef6e41e9093ae1121f5e8dbbf4b1c
Adhurim Muqiqi <adhurimmuqiqi1995@gmail.com>
2023-06-23 21:11:36 +0200

    Setup React app with Vite bundler, integrated Tailwind CSS for styling, organized project folders, and added .gitignore excluding node_modules.

---
+++ monorepo/React/react-task/src/services/optionsData.js
@@ -0,0 +1,21 @@
+import smiley2 from '../assets/smiley2.png'
+import smiley3 from '../assets/smiley3.jpg'
+import smiley4 from '../assets/smiley4.jpg'
+
+export const options = [
+    { id: 1, name: 'Dermal Fillers', photo: smiley3 },
+    { id: 2, name: 'Secret RF', photo: smiley4 },
+    { id: 3, name: 'Facials', photo: smiley2 },
+    { id: 4, name: 'Led', photo: smiley3 },
+    { id: 5, name: 'Fat Dissolve', photo: smiley2 },
+    { id: 6, name: 'ProfHilo', photo: smiley2 },
+    { id: 7, name: 'Growth Factors', photo: smiley2 },
+    { id: 8, name: 'HArmonyCA', photo: smiley2 },
+    { id: 9, name: 'Special Package for Time Clinic', photo: smiley2 },
+    { id: 10, name: 'Consulation', photo: smiley2 },
+  ];
+
+  export default options;
+
+ 
+  
